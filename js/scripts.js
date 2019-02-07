728ab1cc57bce33fa0ee392974655a44499e3ada
Yyyyaaaannnnoooo <yannpatrickmartins@gmail.com>
2019-02-07 17:54:54 +0100

    update index css and add image slide show

--- js/scripts.js
+++ js/scripts.js
@@ -40,7 +40,7 @@ function extractImgs(data) {
     Object.keys(data).forEach(key => {
         result.push(data[key].URL);
     })
-    console.log(result);
+    // console.log(result);
     return result;
 }
 // let data;
@@ -115,10 +115,38 @@ function render_web_page(content) {
             // here we create the div containing the html text
             const content_text = document.createElement('div');
             content_text.setAttribute('class', 'content-text');
+
             const regex = /\<img[^>]*>/g;//regex to replace img tags
             const HTMLText = content[project][title].text.replace(regex, '');// here we do the replacement
             content_text.innerHTML = HTMLText;// here we insert the text
             content_div.appendChild(content_text);//append text tpo the div
+
+            // here we create our image container with the slide show
+
+            const img_urls = content[project][title].imgs;
+            const img_id = project + '-' + title;
+            if (img_urls.length > 0) {
+                const content_imgs = document.createElement('div');
+                content_imgs.setAttribute('class', 'content-img');
+                const img = document.createElement('img');
+                img.setAttribute('src', img_urls[0]);
+                img.setAttribute('id', img_id)
+                content_imgs.appendChild(img);
+                // here we do the next button that changes the images on click
+                const next_img = document.createElement('div');
+                next_img.innerText = 'NEXT IMAGE ⏭';
+
+                let img_idx = 0;
+                next_img.addEventListener('click', (event) => {
+                    img_idx++
+                    if (img_idx >= img_urls.length) img_idx = 0;
+                    img.setAttribute('src', img_urls[img_idx]);
+                });
+                next_img.setAttribute('class', 'next-btn');
+
+                content_imgs.appendChild(next_img);
+                content_text.insertBefore(content_imgs, content_text.firstChild);
+            }
 
 
             // here we create the header of each project
@@ -137,35 +165,7 @@ function render_web_page(content) {
             close_btn.onclick = () => $(content_div).toggle('fast');
             header_btns.appendChild(close_btn);
 
-            // const enlarge_btn = document.createElement('div');
-            // enlarge_btn.setAttribute('class', 'enlarge-button');
-            // enlarge_btn.onclick = () => {
-            //     $(content_div).width(innerWidth - 100).height(innerHeight - 100).css({ top: 50, left: 50 });
-            //     // content_text.style.height
-            // }
-            // header_btns.appendChild(enlarge_btn);
-
-            // const reduce_btn = document.createElement('div');
-            // reduce_btn.setAttribute('class', 'reduce-button');
-            // reduce_btn.onclick = () => {
-            //     $(content_div).width(300).height(innerHeight * 0.65);
-            // };
-            // header_btns.appendChild(reduce_btn);
-
             header.insertBefore(header_btns, header.firstChild);//insert buttons
-            // here we create our image container
-
-            const imgs = content[project][title].imgs;
-            if (imgs.length > 0) {
-                const content_imgs = document.createElement('div');
-                content_imgs.setAttribute('class', 'content-img');
-                for (const url of imgs) {
-                    const img = document.createElement('img');
-                    img.setAttribute('src', url);
-                    content_imgs.appendChild(img);
-                }
-                content_text.appendChild(content_imgs);
-            }
 
             content_div.insertBefore(header, content_div.firstChild);//insert header
             const id_name = 'content' + id_index;
@@ -174,7 +174,7 @@ function render_web_page(content) {
 
             } else {
                 const w = Math.floor(Math.random() * (innerWidth - 300));//correct this!
-                const h = Math.floor(Math.random() * (innerHeight - 300));
+                const h = Math.floor(Math.random() * innerHeight * 0.3);
                 content_div.style.top = h + 'px';
                 content_div.style.left = w + 'px';
             }
@@ -192,7 +192,7 @@ function render_web_page(content) {
                 $(content_div).toggle('fast');
             }
             el_list.appendChild(list_element);
-            console.log(project, title);
+            // console.log(project, title);
             id_index++;
         });
         index++;
@@ -231,10 +231,10 @@ function render_web_page(content) {
          * here we animate the svg path
          * 
          *******************************/
-        const rand_nums = [];
-        for (let i = 0; i < 22; i++)rand_nums.push(10 + Math.floor(Math.random() * 81));
-        const d = `M${rand_nums[0]},${rand_nums[1]} Q${rand_nums[2]},${rand_nums[3]} ${rand_nums[4]},${rand_nums[5]} Q${rand_nums[6]},${rand_nums[7]} ${rand_nums[8]},${rand_nums[9]} Q${rand_nums[10]},${rand_nums[11]} ${rand_nums[12]},${rand_nums[13]} Q${rand_nums[14]},${rand_nums[15]} ${rand_nums[16]},${rand_nums[17]} Q${rand_nums[18]},${rand_nums[19]} ${rand_nums[20]},${rand_nums[21]}`;
-        for (let i = 1; i <= 4; i++)document.getElementById('path' + i).setAttribute('d', d);
+        // const rand_nums = [];
+        // for (let i = 0; i < 22; i++)rand_nums.push(10 + Math.floor(Math.random() * 81));
+        // const d = `M${rand_nums[0]},${rand_nums[1]} Q${rand_nums[2]},${rand_nums[3]} ${rand_nums[4]},${rand_nums[5]} Q${rand_nums[6]},${rand_nums[7]} ${rand_nums[8]},${rand_nums[9]} Q${rand_nums[10]},${rand_nums[11]} ${rand_nums[12]},${rand_nums[13]} Q${rand_nums[14]},${rand_nums[15]} ${rand_nums[16]},${rand_nums[17]} Q${rand_nums[18]},${rand_nums[19]} ${rand_nums[20]},${rand_nums[21]}`;
+        // for (let i = 1; i <= 4; i++)document.getElementById('path' + i).setAttribute('d', d);
 
     }
 
